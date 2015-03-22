856897d7c0540f24cf55405f53285ecfc2651b4b
hcwhan <hcwhan@qq.com>
2015-03-22 22:40:59 +0800

    acfunfix-

---
+++ acfunfix-.user.js
@@ -0,0 +1,29 @@
+﻿// ==UserScript==
+// @name        AcFunFix-
+// @namespace   http://www.talkshowcn.com/js/acfunfix.html
+// @description AcFunFix-
+// @include     http://www.acfun.tv/v/*
+// @include     http://acfun.tv/v/*
+// @include     http://hengyang.acfun.tv/v/*
+// @include     http://wenzhou.acfun.tv/v/*
+// @version     1.0.0.0
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+(function ($) {
+  window._doFix = function () {
+    var f = document.createElement('script');
+    f.src = 'https://raw.githubusercontent.com/hcwhan/AcFunFix-/master/acfunfix-.js?ran=' + new Date().getTime();
+    document.body.appendChild(f);
+    //直接加载远程脚本，防止本地脚本失效。
+  }
+  window._waitPlayer = function () {
+    if (document.getElementById('ACFlashPlayer-re') && window.$) {
+      clearInterval(_waitInt);
+      _doFix();
+    }
+  }
+  _waitInt = setInterval('_waitPlayer()', 500);
+}) (function ($) {
+  return document.querySelector($);
+});
