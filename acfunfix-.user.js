5febcb5992b443bfc5539dfa37ac903b72964c94
hcwhan <hcwhan@qq.com>
2015-07-10 16:31:12 +0800

    Update acfunfix-.user.js

--- acfunfix-.user.js
+++ acfunfix-.user.js
@@ -8,7 +8,7 @@
 // @include     http://wenzhou.acfun.tv/v/*
 // @include     http://www.acfun.tv/a/*
 // @include     http://acfun.tv/a/*
-// @version     1.0.0.2
+// @version     1.0.0.3
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
